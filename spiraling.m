335fdf909358443f11d4a2112d59bd58a897df1a
Matt Mader <mattmaderCT@gmail.com>
2022-02-24 05:47:58 -0500

    annotated spiraling

--- spiraling.m
+++ spiraling.m
@@ -1,26 +1,44 @@
-function [tof,dV] = spiraling(propulsion,initial_orbit,final_orbit,mass)
+function [tof,dV] = spiraling(propulsion,initial_orbit,final_orbit)
 % assumptions:
 % either solar sail or ion
 % for solar sail use same values as POLARIS
 
-    mu = 132712440018;  % [km^3/s^2] solar gravitational parameter
+% constants
+mu = 132712440018;  % [km^3/s^2] solar gravitational parameter
 
-    initial_radius = initial_orbit.perihelion; % [km] assuming circular 
-    desired_radius = final_orbit.perihelion; % [km] end at desired perihelion
+% assuming starting and ending circular orbits ***** need to change *****
+initial_radius = initial_orbit.perihelion; % [km]
+desired_radius = final_orbit.perihelion; % [km]
+
+% solar sail propulsion
+if propulsion.type == "Solar Sail"
+
+    % delta-V required
+    dV = 0; % [km/s]
+
+    % lightness factor
+    beta = propulsion.beta; % [-] 
+
+    % ?????
+    alpha = atan(1/sqrt(2));
+
+    % time of flight
+    tof = 1/3 * abs(desired_radius^1.5 - initial_radius^1.5) * ...
+        sqrt( (1-beta*(cos(alpha))^3)/(beta^2*mu*(cos(alpha))^4*...
+        (sin(alpha))^2) ) / 86400; % [days]
+
+% ion engine propulsion
+elseif propulsion.type == "Ion"
     
-    if propulsion.name == "Solar Sail"
-        % S_0 = affordable_budget * 0.8 / price_per_area;
-        % a_c = 8.172 / (M_total * 1000) * S_0;   % M_total (kg), S_0(m^2)
-        % beta = a_c / 5.93;
-        dV = 0; % [km/s]
-        beta = propulsion.beta; % [-] lightness factor
-        alpha = atan(1/sqrt(2));
-        tof = 1/3 * abs(desired_radius^1.5 - initial_radius^1.5) * sqrt( (1-beta*(cos(alpha))^3)/(beta^2*mu*(cos(alpha))^4*(sin(alpha))^2) ) / 86400; % days
-            
-    elseif propulsion.name == "Ion"
-       dV = sqrt(mu/desired_radius) - sqrt(mu/initial_radius);
-       T = 1.77 / mass.prev.total / 1000; % acceleration from ion engine, km/s^2
-       tof = dV/ T / 86400;
-    end
+    % delta-V required
+    dV = sqrt(mu/desired_radius) - sqrt(mu/initial_radius); % [km/s]
 
-end
+    % ion engine acceleration
+    T = propulsion.accel/1000; % [km/s^2]
+
+    % time of flight
+    tof = dV / T / 86400; % [days]
+
+end % if/elseif
+
+end % function
